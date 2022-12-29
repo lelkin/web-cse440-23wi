92ed97c0f9b1d2875aec12787fe2c6fc6d3ca6b7
James Fogarty <jayfo@users.noreply.github.com>
2022-12-29 11:14:20 -0800

    Website Components

---
+++ src/components/CollapseProjectMilestone.tsx
@@ -0,0 +1,48 @@
+import {
+    observer
+} from "mobx-react-lite";
+
+import {
+    Stack,
+} from "@mui/material";
+
+import * as React from "react";
+
+import {
+    CollapseWithHeader
+} from 'src/common/CollapseWithHeader';
+
+import {
+    anchorText
+} from 'src/common/GeneratedAnchor';
+
+import {
+    CourseInformationDueDate,
+} from 'src/components/CourseInformation';
+
+import {
+    CourseInformationStore
+} from "src/stores/CourseInformationStore";
+
+//
+// Properties
+//
+interface CollapseProjectMilestoneProps extends React.PropsWithChildren<{}> {
+    heading: string,
+    dueDateName: keyof CourseInformationStore,
+}
+
+/**
+ */
+export const CollapseProjectMilestone: React.FunctionComponent<CollapseProjectMilestoneProps> = observer((props: CollapseProjectMilestoneProps) => {
+    return <CollapseWithHeader header={
+        <Stack>
+            <h3 id={anchorText(props.heading)}>{props.heading}</h3>
+            <p><CourseInformationDueDate dueDateName={props.dueDateName} /></p>
+        </Stack>
+    }>
+        {props.children}
+    </CollapseWithHeader>
+});
+
+export default CollapseProjectMilestone;
---
+++ src/components/ProjectImageBlock.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import ProjectImagesBlock from "src/components/ProjectImagesBlock";
+
+interface ProjectImage {
+    src: string;
+    caption?: string;
+}
+
+interface ProjectImageBlockProps {
+    children: React.ReactNode,
+    image: ProjectImage;
+    caption?: string;
+    width: 4 | 6 | 8;
+    maxHeight?: string;
+}
+
+export const ProjectImageBlock: React.FunctionComponent<ProjectImageBlockProps> = (props) => {
+    return (
+        <ProjectImagesBlock
+            images={[props.image]}
+            caption={props.caption}
+            direction={"row"}
+            width={props.width}
+            maxHeight={props.maxHeight}
+        >
+            { props.children }
+        </ProjectImagesBlock>
+    );
+}
+
+export default ProjectImageBlock;
